6d8d8244775c493171a9b3b5b9485091611ce11f
Dan Hon <dan@danhon.com>
2024-12-06 14:40:35 -0800

    um completely refactored?

--- test.py
+++ test.py
@@ -2,17 +2,30 @@ import os
 import pprint
 import json
 from dotenv import load_dotenv
-from utils import generate_post, generate_post_short, get_rules
+from utils import generate_posts, get_rules, mastodon_client, bluesky_instance, bluesky_faceted_post
 
 
-# Load environment variables
+# Load and set environment variables
 load_dotenv()
 
 # set grammars directory and file
 GRAMMARS_DIRECTORY = os.getenv("GRAMMARS_DIRECTORY")
 GRAMMAR_JSON = "starfleetjobs.json"
 
-rules = get_rules(GRAMMARS_DIRECTORY, GRAMMAR_JSON)
-post = generate_post_short(rules)
+def post_to_mastodon(post):
+    mastodon_instance = mastodon_client()
+    mastodon_instance.status_post(post["long"])
 
-pprint.pp(post)
+def post_to_bluesky(post):
+    bluesky_client = bluesky_instance()
+    bluesky_post = bluesky_faceted_post(post["short"])
+    bluesky_client.post(bluesky_post)
+
+# get our tracery rules
+rules = get_rules(GRAMMARS_DIRECTORY, GRAMMAR_JSON)
+
+# generate our posts dict containing a short post and a long post
+post = generate_posts(rules)
+
+post_to_mastodon(post)
+post_to_bluesky(post)
--- utils.py
+++ utils.py
@@ -1,30 +1,91 @@
+import os
+from dotenv import load_dotenv
+import pprint
+import json
+import re
+
 import tracery
 from tracery.modifiers import base_english
 
-import json
-import os
+from mastodon import Mastodon
+from atproto import Client, client_utils
+
+def generate_posts(rules):
+    post = {}
+    
+    post["long"] = generate_post(rules)
+    post["short"] = generate_post_short(rules)
+    
+    # pprint.pp(len(post["short"]))
+    # pprint.pp(len(post["long"]))
+    
+    return post
 
 def generate_post(rules):
     grammar = tracery.Grammar(rules)
     grammar.add_modifiers(base_english)
-    post = grammar.flatten("#origin#")
-    return post
+    long_post = grammar.flatten("#origin#")
+    return long_post
 
 def generate_post_short(rules):
-    grammar = tracery.Grammar(rules)
-    grammar.add_modifiers(base_english)
-    post = grammar.flatten("#origin#")
+    short_post = generate_post(rules)
 
-    while len(post) > 260:
-        post = grammar.flatten("#origin#")
-        if len(post) < 260:
+    while len(short_post) > 260:
+        short_post = generate_post(rules)
+        if len(short_post) < 260:
             break
-    return post
+    return short_post
 
 def get_rules(GRAMMARS_DIRECTORY, GRAMMAR_JSON):
     file_path = os.path.join(GRAMMARS_DIRECTORY, GRAMMAR_JSON)
 
     with open(file_path) as rules_file:
         rules = json.load(rules_file)
-
     return rules
+
+def mastodon_client():
+    environment = envs()
+    MASTODON_TOKEN = environment["MASTODON_TOKEN"]
+    MASTODON_BASE_URL = environment["MASTODON_BASE_URL"]
+
+    mastodon_instance = Mastodon(
+        access_token=MASTODON_TOKEN,
+        api_base_url=MASTODON_BASE_URL
+    )
+
+    return mastodon_instance
+
+def bluesky_instance():
+    environment = envs()
+    BLUESKY_USERNAME = environment["BLUESKY_USERNAME"]
+    BLUESKY_PASSWORD = environment["BLUESKY_PASSWORD"]
+
+    client = Client("https://bsky.social")
+
+    client.login(BLUESKY_USERNAME, BLUESKY_PASSWORD)
+    print("generated a client")
+    return client
+
+def bluesky_faceted_post(post):
+    post_without_tags = post.split('#')[0]
+
+    text_builder = client_utils.TextBuilder()
+    text_builder.text(post_without_tags)
+
+    # parse the hashtags
+    hashtags = re.findall(r"#\w+", post)
+    # pprint.pp(hashtags)
+ 
+    # add tags 
+    for tag in hashtags:
+        # Add each hashtag as a tag facet
+        text_builder.tag(tag + " ", tag.split('#')[1])
+
+    return text_builder
+
+
+
+
+def envs():
+    load_dotenv()
+    return os.environ
